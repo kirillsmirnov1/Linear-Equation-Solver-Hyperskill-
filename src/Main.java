e4716beb01464f9ee2cec35825da47e37bbe6324
trulden <thegaldren@gmail.com>
2019-01-31 14:24:05 +0300

    Stage 2

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,14 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         double a = sc.nextDouble();
         double b = sc.nextDouble();
-        System.out.println( (b*1d) / (a*1d) );
+        double c = sc.nextDouble();
+        double d = sc.nextDouble();
+        double e = sc.nextDouble();
+        double f = sc.nextDouble();
+
+        double y = (f - c*d/a)/(e-b*d/a);
+        double x = (c - b*y)/a;
+        
+        System.out.println(x + " " + y);
     }
 }
