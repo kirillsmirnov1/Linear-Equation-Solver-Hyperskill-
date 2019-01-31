35b2ed11c6d97cfc3c440d293d6957e1f8bd83c1
trulden <thegaldren@gmail.com>
2019-01-31 13:50:31 +0300

    Stage 1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        System.out.println( (b*1d) / (a*1d) );
+    }
+}
